41da75a9f683d145b7b92c0217b560a42531153b
Petar Simonovic <simonovicp12@gmail.com>
2022-05-17 11:34:45 +0200

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
+Route::get('/post', [App\Http\Controllers\PostController::class, 'index'])->name('post');
