8d82c63af070f47ab157b6ab8dc21e2155aadc5c
Petar Simonovic <simonovicp12@gmail.com>
2022-05-17 19:43:32 +0200

    passing all post data to desired view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
 
     public function index(){
         # code
-        return view('admin.posts.index');
+        $posts = Post::all();
+        return view('admin.posts.index', ['posts'=>$posts]);
     }
 
     public function show(Post $post){
