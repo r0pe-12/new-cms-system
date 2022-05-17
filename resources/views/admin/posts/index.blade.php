86522d439184e04da5f11ebb8ef31c7977cc952f
Petar Simonovic <simonovicp12@gmail.com>
2022-05-17 18:39:52 +0200

    Added data tables to index.blade.php

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,21 @@
 <x-admin-master>
+
     @section('content')
         <h1>All Posts</h1>
+
+        <x-admin-posts-table>
+
+        </x-admin-posts-table>
+
     @endsection
+
+    @section('scripts')
+        <!-- Page level plugins -->
+        <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+        <!-- Page level custom scripts -->
+        <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
+    @endsection
+
 </x-admin-master>
