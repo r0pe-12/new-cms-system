70cacf263ebf550a76a89df672bba268dee44d0c
Petar Simonovic <simonovicp12@gmail.com>
2022-05-24 00:37:34 +0200

    Controller and route for displaying user profile with data passed to views

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+
+    public function show(User $user){
+        # code
+        return view('admin.users.profile', ['user'=>$user]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/post/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('post');
 
 Route::middleware('auth')->group(function (){
 
-    Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin', [\App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
 
 //    Displaying create form
     Route::get('/admin/posts/create', [\App\Http\Controllers\PostController::class, 'create'])->name('post.create');
@@ -35,5 +35,8 @@ Route::middleware('auth')->group(function (){
     Route::get('/admin/posts/{post}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->middleware('can:view,post')->name('post.edit');
 //    updating post
     Route::patch('/admin/posts/{post}/update', [\App\Http\Controllers\PostController::class, 'update'])->name('post.update');
+
+//    display user
+    Route::get('admin/user/{user}/profile', [\App\Http\Controllers\UserController::class, 'show'])->name('user.profile.show');
 
 });
